59f4bdc2bbbf2fb38c475b796e9da969c478dbbd
Gaurav Joshi <gauravjoshi.7286@gmail.com>
2018-07-27 16:28:10 +0530

    Updated service endpoint URLs

--- src/app/constants/endpointUrls.ts
+++ src/app/constants/endpointUrls.ts
@@ -1,8 +1,9 @@
 const endPointDomain = 'http://localhost:3000';
 
 export class EndpointUrls {
-    public static GET_SUPERVISEES = '/assets/mock/getSupervisees.json';
+    public static GET_SUPERVISEES = `${endPointDomain}/supervisees/{encOId}`;
     public static GET_SUPERVISEE_DETAILS = '/assets/mock/getSuperviseeDetails.json';
+
 
     public static GET_NOMINATIONS = `${endPointDomain}/nomination`;
     public static UPDATE_NOMINATION_STATUS = `${endPointDomain}/nomination/{id}`;
@@ -10,8 +11,6 @@ export class EndpointUrls {
     public static SUBMIT_NOMINATION = `${endPointDomain}/nomination`;
     public static EXPORT_NOMINATIONS = `${endPointDomain}/nomination/export`;
 
-    // public static GET_USER_DETAILS = `${endPointDomain}/users/{encOId}`;
-    public static GET_USER_DETAILS = `${endPointDomain}/users`;
     public static GET_USERS = `${endPointDomain}/users`;
     public static ADD_USER = `${endPointDomain}/users`;
     public static UPDATE_USER = `${endPointDomain}/users/{id}`;
@@ -20,5 +19,5 @@ export class EndpointUrls {
     public static NOTIFICATION_SERVICE = `${endPointDomain}/subscribe`;
     public static PUBLIC_VAPID_KEY = 'BAMk7loC8oRci8CsezXqjk7H777nM08RbJsT8J87THq1Qgf7Udgv8awrjSZhYAOgWZRiucrCgowXzawDsZGYcHg';
 
-    public static EMAIL_LINK = `${endPointDomain}/mailLink`;
+    public static EMAIL_LINK = `${endPointDomain}/sendMail`;
 }
